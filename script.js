02c01421bd440074e192e9c0cf1e92dd98192dd7
patricia prager <florence.voltaire@web.de>
2022-01-06 08:45:03 +0100

    function updateState script.js

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function render() {
 }
 
 function updateState() {
+    // click in newTodo = schreiben einer Task mit mind. 5 Zeichen im Eingabefeld
 
+    // click the addButton = die geschriebene Aufgabe wird der Liste hinzugefügt
+const newTodoTask = document.querySelector(addButton).addEventListener('click', )
+    // click the buttonRemove = alle Todo-Tasks werden gelöscht
+const removeButton = document.querySelector(buttonRemove).addEventListener('click', clearAll )
+    // Anwählen der filterCheckbox = treffen einer Auswahl der angezeigten Werte
+
+    // click the checkedBox = abhaken der erfüllten Aufgabe (Task isDone)
+   
 }
